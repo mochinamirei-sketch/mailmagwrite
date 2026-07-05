51a499bbcf06fa734ca8e3b6f3ad8e6710ecf44c
mochinamirei-sketch <mochinamirei@gmail.com>
2026-07-05 10:38:58 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == st.secrets.get("APP_PASSWORD", ""):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("パスワード", type="password", on_change=password_entered, key="password")
+        st.stop()
+    elif not st.session_state["password_correct"]:
+        st.text_input("パスワード", type="password", on_change=password_entered, key="password")
+        st.error("パスワードが違います")
+        st.stop()
+
+check_password()
 import json
 import uuid
 import copy
